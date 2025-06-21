5759ff45b73f381e5f1c15a67d5f9c15d1cc5be6
Thomas <thomas.kottenhahn@gmx.de>
2025-06-21 22:55:18 +0200

    stopped tracking target directory

--- target/debug/deps/cfg_if-885ced7a34e6dac7.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\libcfg_if-885ced7a34e6dac7.rmeta: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\libcfg_if-885ced7a34e6dac7.rlib: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\cfg_if-885ced7a34e6dac7.d: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.0\src\lib.rs:
--- target/debug/deps/rand_chacha-1e4d6ef25fcea7b5.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\librand_chacha-1e4d6ef25fcea7b5.rmeta: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\lib.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\librand_chacha-1e4d6ef25fcea7b5.rlib: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\lib.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\Thoma\OneDrive - JGU\correspondence-arcs\target\debug\deps\rand_chacha-1e4d6ef25fcea7b5.d: C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\lib.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\chacha.rs C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\guts.rs
-
-C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\lib.rs:
-C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\chacha.rs:
-C:\Users\Thoma\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_chacha-0.3.1\src\guts.rs:
